67b66fd5fce6a0df86c44ced0ccd7438fa3905f9
Red-French <red@redfrench.net>
2017-03-26 17:06:49 -0500

    successfully displaying data to user

--- app/src/main/java/net/redfrench/volleyauth/UserAreaActivity.java
+++ app/src/main/java/net/redfrench/volleyauth/UserAreaActivity.java
@@ -1,5 +1,6 @@
 package net.redfrench.volleyauth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -15,5 +16,15 @@ public class UserAreaActivity extends AppCompatActivity {
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etAge = (EditText) findViewById(R.id.etAge);
         final TextView welcomeMsg = (TextView) findViewById(R.id.tvWelcome);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String username = intent.getStringExtra("username");
+        int age = intent.getIntExtra("age", -1);  // getIntExtra requires initializing int value (-1 will indicate a value wasn't passed)
+
+        String message = name + ", welcome to your user area.";
+        welcomeMsg.setText(message);
+        etUsername.setText(username);
+        etAge.setText(age + "");  // can't display int in a text field directly, so add empty string to convert this to a string
     }
 }
